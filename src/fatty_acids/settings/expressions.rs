7b5555d53fe35521012d82f5322672c2944b5c28
kgv <kgv@users.noreply.github.com>
2026-05-29 16:46:17 +0300

    1

--- src/fatty_acids/settings/expressions.rs
+++ src/fatty_acids/settings/expressions.rs
@@ -2,7 +2,7 @@ use crate::settings::Array;
 use const_format::formatcp;
 use egui::Ui;
 use egui_l10n::prelude::*;
-use fatty_acid_expressions::r#const::{BIODIESEL, METABOLIC, NUTRITIONAL, SUM, ratio, sum};
+use fatty_acid_expressions::r#const::{BIODIESEL, METABOLIC, NUTRITIONAL, PREFIX, SUM, ratio, sum};
 use serde::{Deserialize, Serialize};
 
 /// Expressions
@@ -19,28 +19,16 @@ impl Expressions {
             ratio: Ratio::new(),
         }
     }
+}
 
+impl Expressions {
     pub fn show(&mut self, ui: &mut Ui) {
         ui.horizontal(|ui| {
-            ui.label(ui.localize(SUM))
-                .on_hover_localized(formatcp!("{SUM}.hover"));
+            ui.label(ui.localize(formatcp!("{PREFIX}_{SUM}")))
+                .on_hover_localized(formatcp!("{PREFIX}_{SUM}.hover"));
             self.sum.show(ui);
         });
-        ui.horizontal(|ui| {
-            ui.label(ui.localize(BIODIESEL))
-                .on_hover_localized(formatcp!("{BIODIESEL}.hover"));
-            self.ratio.biodiesel.show(ui);
-        });
-        ui.horizontal(|ui| {
-            ui.label(ui.localize(METABOLIC))
-                .on_hover_localized(formatcp!("{METABOLIC}.hover"));
-            self.ratio.metabolic.show(ui);
-        });
-        ui.horizontal(|ui| {
-            ui.label(ui.localize(NUTRITIONAL))
-                .on_hover_localized(formatcp!("{NUTRITIONAL}.hover"));
-            self.ratio.nutritional.show(ui);
-        });
+        self.ratio.show(ui);
     }
 }
 
@@ -59,5 +47,25 @@ impl Ratio {
             metabolic: Array::from(ratio::metabolic::RATIOS),
             nutritional: Array::from(ratio::nutritional::RATIOS),
         }
+    }
+}
+
+impl Ratio {
+    pub fn show(&mut self, ui: &mut Ui) {
+        ui.horizontal(|ui| {
+            ui.label(ui.localize(formatcp!("{PREFIX}_{BIODIESEL}")))
+                .on_hover_localized(formatcp!("{PREFIX}_{BIODIESEL}.hover"));
+            self.biodiesel.show(ui);
+        });
+        ui.horizontal(|ui| {
+            ui.label(ui.localize(formatcp!("{PREFIX}_{METABOLIC}")))
+                .on_hover_localized(formatcp!("{PREFIX}_{METABOLIC}.hover"));
+            self.metabolic.show(ui);
+        });
+        ui.horizontal(|ui| {
+            ui.label(ui.localize(formatcp!("{PREFIX}_{NUTRITIONAL}")))
+                .on_hover_localized(formatcp!("{PREFIX}_{NUTRITIONAL}.hover"));
+            self.nutritional.show(ui);
+        });
     }
 }
