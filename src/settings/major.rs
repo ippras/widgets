ef6f522969d3ce3d341215959e7019ee14c6594d
kgv <kgv@users.noreply.github.com>
2026-05-31 21:03:19 +0300

    2

--- src/settings/major.rs
+++ src/settings/major.rs
@@ -12,6 +12,7 @@ use egui::{ComboBox, PopupCloseBehavior, Slider, SliderClamping, Ui, Widget};
 use egui_l10n::prelude::*;
 use egui_phosphor::regular::BOOKMARK;
 use ordered_float::OrderedFloat;
+use polars::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::iter::zip;
 use typed_builder::TypedBuilder;
@@ -65,8 +66,7 @@ impl Major {
                     if percent {
                         value *= 100.0;
                     }
-                    // AnyValue::Float64(value).to_string()
-                    value.to_string()
+                    AnyValue::Float64(value).to_string()
                 })
                 .custom_parser(|value| {
                     let mut parsed = value.parse().ok()?;
