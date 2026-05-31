ca2976ce1f796da9df0cbd16d0e77924944ef4ed
kgv <kgv@users.noreply.github.com>
2026-05-31 08:29:26 +0300

    3

--- src/fatty_acids/settings/mod.rs
+++ src/fatty_acids/settings/mod.rs
@@ -1,3 +1,14 @@
 pub use self::expressions::Expressions;
 
+use serde::{Deserialize, Serialize};
+
+/// Settings
+#[derive(Clone, Debug, Deserialize, Hash, PartialEq, Serialize)]
+pub struct Settings {
+    pub threshold: bool,
+    pub sort: bool,
+    pub filter: bool,
+    pub expressions: Expressions,
+}
+
 pub mod expressions;
