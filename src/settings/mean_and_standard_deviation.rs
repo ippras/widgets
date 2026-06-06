3839dd949ba03134d8fddcb471d2513171cc9f0b
kgv <kgv@users.noreply.github.com>
2026-06-06 22:41:19 +0300

    3

--- src/settings/mean_and_standard_deviation.rs
+++ src/settings/mean_and_standard_deviation.rs
@@ -12,19 +12,36 @@ use serde::{Deserialize, Serialize};
 /// Mean and standard deviation
 #[derive(Clone, Copy, Debug, Default, Deserialize, EguiProbe, Hash, PartialEq, Serialize)]
 #[egui_probe(name = ui.localize(formatcp!("{PREFIX}_{MEAN}")))]
+pub struct _MeanAndStandardDeviation {
+    #[egui_probe(name = _ui.localize(formatcp!("{PREFIX}_{STANDARD_DEVIATION}")))]
+    pub standard_deviation: Option<StandardDeviation>,
+}
+
+/// Mean
+#[derive(Clone, Copy, Debug, Default, Deserialize, EguiProbe, Hash, PartialEq, Serialize)]
+#[egui_probe(name = ui.localize(formatcp!("{PREFIX}_{MEAN}")))]
 pub struct Mean {
     #[egui_probe(name = _ui.localize(formatcp!("{PREFIX}_{STANDARD_DEVIATION}")))]
     pub standard_deviation: Option<StandardDeviation>,
 }
 
 /// Standard deviation
-#[derive(Clone, Copy, Debug, Default, Deserialize, EguiProbe, Hash, PartialEq, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, EguiProbe, Hash, PartialEq, Serialize)]
 #[egui_probe(name = ui.localize(formatcp!("{PREFIX}_{STANDARD_DEVIATION}")))]
 pub struct StandardDeviation {
     #[egui_probe(name = _ui.localize(formatcp!("{PREFIX}_{KIND}")))]
     pub kind: Kind,
     #[egui_probe(name = _ui.localize(formatcp!("{PREFIX}_{DELTA_DEGREES_OF_FREEDOM}")), range = 0..=1)]
     pub ddof: u8,
+}
+
+impl Default for StandardDeviation {
+    fn default() -> Self {
+        Self {
+            kind: Kind::Absolute,
+            ddof: 1,
+        }
+    }
 }
 
 /// Mean and standard deviation
