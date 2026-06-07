d190c0d3fce3b6b1dad86d7e440cbbe4f8596672
kgv <kgv@users.noreply.github.com>
2026-06-07 14:45:29 +0300

    1

--- src/probes/precision_and_significant.rs
+++ src/probes/precision_and_significant.rs
@@ -2,7 +2,7 @@ use crate::r#const::{PERCENT, PRECISION, PREFIX, SIGNIFICANT};
 use const_format::formatcp;
 use egui::{Slider, Ui, Widget};
 use egui_l10n::ContextExt as _;
-use egui_phosphor::regular::{BOOKMARK, DOTS_THREE};
+use egui_phosphor::regular::{BOOKMARK, DOTS_THREE_OUTLINE};
 use egui_probe::EguiProbe;
 use serde::{Deserialize, Serialize};
 use typed_builder::TypedBuilder;
@@ -12,7 +12,7 @@ pub const MAX_PRECISION: usize = 16;
 
 /// Precision and significant
 #[derive(Clone, Copy, Debug, Deserialize, EguiProbe, Hash, PartialEq, Serialize)]
-#[egui_probe(name = DOTS_THREE)]
+#[egui_probe(name = DOTS_THREE_OUTLINE)]
 pub struct PrecisionAndSignificant {
     #[egui_probe(name = _ui.localize(formatcp!("{PREFIX}_{PRECISION}")), range = 1..=MAX_PRECISION, bookmarks = [1, 3])]
     pub precision: usize,
