a93055864683ca6d068c259b8799d8f34c7b5922
kgv <kgv@users.noreply.github.com>
2026-06-07 11:54:41 +0300

    Fix precision

--- src/settings/precision.rs
+++ src/settings/precision.rs
@@ -3,11 +3,30 @@ use const_format::formatcp;
 use egui::{Slider, Ui, Widget};
 use egui_l10n::ContextExt as _;
 use egui_phosphor::regular::BOOKMARK;
+use egui_probe::EguiProbe;
 use serde::{Deserialize, Serialize};
 use typed_builder::TypedBuilder;
 
 /// IEEE 754-2008
 pub const MAX_PRECISION: usize = 16;
+
+/// Precision
+#[derive(Clone, Copy, Debug, Deserialize, EguiProbe, Hash, PartialEq, Serialize)]
+pub struct PrecisionAndSignificant {
+    #[egui_probe(name = _ui.localize(formatcp!("{PREFIX}_{PRECISION}")), range = 1..=MAX_PRECISION)]
+    pub precision: usize,
+    #[egui_probe(name = _ui.localize(formatcp!("{PREFIX}_{SIGNIFICANT}")))]
+    pub significant: bool,
+}
+
+impl Default for PrecisionAndSignificant {
+    fn default() -> Self {
+        Self {
+            precision: 1,
+            significant: false,
+        }
+    }
+}
 
 /// Precision
 #[derive(Clone, Copy, Debug, Deserialize, Hash, PartialEq, Serialize, TypedBuilder)]
