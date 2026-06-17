b5882455de9dd27e9faec4965ff5677e117aaf1d
kgv <kgv@users.noreply.github.com>
2026-06-17 12:59:51 +0300

    2

--- src/settings/precision_and_significant.rs
+++ src/settings/precision_and_significant.rs
@@ -46,7 +46,7 @@ impl<const N: usize> PrecisionAndSignificant<N> {
             if !self.bookmarks.is_empty() {
                 ui.menu_button(BOOKMARK, |ui| {
                     for bookmark in self.bookmarks {
-                        if ui.button((BOOKMARK, bookmark.to_string())).clicked() {
+                        if ui.button(bookmark.to_string()).clicked() {
                             self.precision = bookmark;
                         }
                     }
